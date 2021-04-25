2ffff894b54bb9b1cc4305c2d729492a60de65cb
master-tecs <masterawahab@gmail.com>
2021-04-25 14:31:40 +0300

    Created login page.

--- src/pages/auth/LoginForm.jsx
+++ src/pages/auth/LoginForm.jsx
@@ -1,10 +1,25 @@
 import TextInput from "../../components/shared/inputs/TextInput";
+import Button from "../../components/shared/buttons/Button";
 import "./LoginForm.scss";
 
 function LoginForm() {
   return (
-    <form action="" className="login">
-      <TextInput name="Email" type="email" />
+    <form action="" className="loginForm">
+      <div className="loginForm__top">
+        <h3>Login</h3>
+        <TextInput name="Email" type="email" />
+        <TextInput name="Password" type="password" />
+        <div>
+          <Button text="Login" />
+        </div>
+      </div>
+      <div className="loginForm__mid">
+        <p>Forget your password?</p>
+      </div>
+      <div className="loginForm__bottom">
+        <p>Don't have an account?</p>
+        <a href="#">Sign in</a>
+      </div>
     </form>
   );
 }
